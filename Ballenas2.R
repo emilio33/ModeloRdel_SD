2d4a8e963b100e2e46d77c521f49668d8848e434
Emilio Rodriguez-Izquierdo <13emil@gmail.com>
2017-11-30 14:15:30 -0600

    Cambios en las embarcaciones
    Se cambió el número de embarcaciones, de forma que ahora hay un valor inicial y se le van añadiendo un número c.t. de embarcaciones cada temporada. Reordenamos el código y se añadieron unas gráficas de los resultados

--- Ballenas2.R
+++ Ballenas2.R
@@ -1,49 +1,52 @@
 library(shiny)
 library(deSolve)
-library (triangle)
-###Definir parámetros
+library (triangle)  ## para poder hacer funciones de probabilidad triangulares
 
+####################################################################
+#### Función para evaluar pérdida de población. Arroja el delta ####
+####################################################################
 
-dB_m <- function(B,tc,K,e,HD){    ##función para evaluar pérdida de población. Arroja el delta
-  R <- 150         ##radio
-  Conv_ha <- 10000 ## xa convertir metros en hectáreas
-  ef <-0.01        ## efecto de disturbio
-  se <- sample(e, 1, replace = TRUE)  ##agarra un valor de embarcaciones
+B_t <- function(tc, B, K, R,  HD, e, ef){     ## Aquí van todos los parámetros (el orden importa) que voy a evaluar
  
-  logis <- tc*B*(1- B/K)                   ##crecimiento logístico de la población
-  hd <- (HD - ((pi*(R*R)/Conv_ha)*se*ef))  ##hábitat que queda disponible
-  cosecha <- (HD - hd)*(B/hd)              ## lo que se pierde de habitat en relacion a las ballenas (hectareas x ballenas/hectareas= ballenas)
-  dB <- logis-cosecha                      ##delta población de ballenas
-  return(list(dB=dB, se=se, tc=tc, logis=logis, hd=hd, cosecha=cosecha))
+  Conv_ha <- 10000                             ## xa convertir metros a hectáreas
+ 
+  logis <- tc*B*(1- B/K)                       ## crecimiento logístico de la población
+  hd <- (HD - ((pi*(R*R)/Conv_ha)*e*ef))       ## hábitat que queda disponible
+  cosecha <- (HD - hd)*(B/hd)                  ## lo que se pierde de habitat en relacion a las ballenas (hectareas x ballenas/hectareas= ballenas)
+  dB <- logis-cosecha                          ## delta población de ballenas
+ 
+   return(list(dB=dB, e=e, tc=tc, logis=logis, hd=hd, cosecha=cosecha))   ## estos son los párametros de los que quiero ver los valores
 }  
 
 
-##Función para calcular la población dinámica
-tiempo <- seq(1,100,length=100)
+#### Función xa calcular la población dinámica ####
 
 Modelo <- function (Poblacion, Y) {
 
-#Inicializar varaibles
-K <- 3000 ##capaciadad de carga
+   ###############################
+   #### Inicializar varaibles ####
+   ###############################
+  
+r <- runif(1000,0.025,0.032)        ## distribución uniforme para la tasa de crecimiento de ballena gris
+#hist (r)                           ## historgrama de la función de probabilidad de la tasa de crecimiento
+sr <- sample(r, 1, replace = TRUE)  ## agarra un valor de tasa de crecimiento
+tc <- sr                            ## valor de la tasa de crecimiento que entra a la función como c.t.
 
-## distribución uniforme para la tasa de crecimiento de ballena gris
-r <- runif(1000,0.025,0.032)
-hist (r)  ##historgrama de la función de probabilidad de la tasa de crecimiento
-sr <- sample(r, 1, replace = TRUE)  ##agarra un valor de tasa de crecimiento
-sr
-tc <- sr
-
-p_0 <- Poblacion
-R <- 150 ##radio
-HABi <- 11000  #habitat inicial en hectáreas
-
-e <- rtriangle(1000,22,100)  ## distribución triangular para el número de embarcaciones
-hist (e)  ##historgrama de la función de probabilidad de las embarcaciones
-
-B <- p_0
+B <- Poblacion
 B_c <- 0
+
+K <- 3000       ## capaciadad de carga
+R <- 150        ## radio de afectación de las embaracaciones
+E <- 22         ## número de embarcaciones inicial
+Eadd <- 2       ## número de embarcaciones adicional cada temporada
+ef <-0.01       ## efecto de disturbio
+
+HABi <- 11000   ## hábitat inicial en hectáreas
 HD <- HABi
-##Inicializar objetos para guardar los resultados de las simulaciones
+
+
+#### Inicializar objetos para guardar los resultados de las simulaciones ####
+
 
 PoblacionBall <- numeric(Y)
 Embarcaciones <- numeric(Y)
@@ -53,23 +56,27 @@ Logis <- numeric(Y)
 Habitat <- numeric(Y)
 Cosecha <- numeric (Y)
 
+     ##########################################################
+     #### creamos un for loop, es decir iteramos el modelo ####
+     ##########################################################
 
-for (y in 1:Y){  ##creamos un for loop, es decir iteramos el modelo
+for (y in 1:Y){  
  
-tmp <- dB_m(B,tc,K,e,HD) ###delta de población, resultado de la ecuación logistica con distintas poblaciones iniciales
-delta <- tmp$dB
-B_c <- B + delta  ##población al tiempo t+1
-B <- B_c ##actualizar el valor de B
-HD <- tmp$hd ##actualizar el valor de HD
+tmp <- B_t(tc,B,K,R,HD,E,ef) ## aquí es donde se actualiza la función B_t (el orden de los factores es importante y debe ser igual al de la función B_t)
+delta <- tmp$dB              ## actualizar el valor de delta, delta de crecimiento de población, resultado de la ecuación logistica con distintas poblaciones iniciales
+B_c <- B + delta             ## población al tiempo t+1
+B <- B_c                     ## actualizar el valor de B
+HD <- tmp$hd                 ## actualizar el valor de HD
+E <- tmp$e + Eadd            ## actualizar el valor de E; embarcaciones en operación (embarcaciones en el tiempo anterior más el factor de embarcaciones adicionales)
 
-
-Delta[y] <- delta
-PoblacionBall[y] <- B
-Embarcaciones [y] <- tmp$se
+          
+Delta [y] <- delta
+PoblacionBall [y] <- B
+Embarcaciones [y] <- E
 TasaCrec [y] <- tmp$tc
-Habitat[y] <- HD
-Logis[y] <-tmp$logis
-Cosecha[y] <- tmp$cosecha
+Habitat [y] <- HD
+Logis [y] <-tmp$logis
+Cosecha [y] <- tmp$cosecha
 
 }
 
@@ -77,16 +84,68 @@ res<-list(Delta=Delta, PoblacionBall=PoblacionBall,
           Embarcaciones=Embarcaciones, TasaCrec=TasaCrec, 
           Logis=Logis, Habitat=Habitat, Cosecha=Cosecha)
 
-return(res)  ##al final nos devuelve los resultados
+return(res)     ## al final nos devuelve los resultados
 }
 
 
-resultadoses1 <- Modelo (1400,100)
+     ###################################################
+     #######         RESULTADOS         ################
+     ###################################################
+
+resultadoses1 <- Modelo (1240,100)
 resultadoses2 <- Modelo (1500,100)
 resultadoses3 <- Modelo (3000,100)
 resultados4 <- Modelo (3500,100)
 
-resultadoses1
+resultadoses1<-as.data.frame(resultadoses1)
+tiempo <- seq(1:length(resultadoses1[,1]))
+plot(tiempo, resultadoses1$PoblacionBall)
+plot(tiempo, resultadoses1$Embarcaciones)
+plot(tiempo, resultadoses1$Habitat)
+
+
+#### Gráfica de ballenas vs tiempo ###########
+
+library(ggplot2)
+library(scales)
+library(reshape2)
+
+ggplot(resultadoses1, aes(x=tiempo, y=PoblacionBall)) + 
+  geom_line(size=1.2) +
+  geom_point(size=3,shape=16) +
+  theme_bw() + 
+  theme(panel.grid.major.y = element_line(linetype="dashed",colour="grey80"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y=element_blank(), 
+        panel.border=element_blank(),
+        axis.line=element_line(colour="black")) +
+  scale_y_continuous (limits=c(0,3000), expand=c(0,0)) +
+  scale_x_continuous(limits=c(0,110), expand=c(0,0)) +
+  labs(x=expression(" Tiempo (años)")) +
+  labs(y=expression("Abundancia de ballenas (individuos)")) 
+
+
+#### Gráfica de ballenas vs hábitat ###########
+
+ggplot(resultadoses1, aes(x=Habitat, y=PoblacionBall)) + 
+  geom_line(size=1.2) +
+  geom_point(size=3,shape=16) +
+  theme_bw() + 
+  theme(panel.grid.major.y = element_line(linetype="dashed",colour="grey80"),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.minor.y=element_blank(), 
+        panel.border=element_blank(),
+        axis.line=element_line(colour="black")) +
+  scale_y_continuous (limits=c(0,3000), expand=c(0,0)) +
+  scale_x_continuous(limits=c(11000,10000), expand=c(11000,10000)) +
+  labs(x=expression("Hábitat disponible (ha)")) +
+  labs(y=expression("Abundancia de ballenas (individuos)")) +
+
+     scale_x_reverse()
+
+
 resultadoses2
 resultadoses3
 resultadoses4
